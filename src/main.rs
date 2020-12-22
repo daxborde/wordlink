e45eac8f70c6241c8f5702871d88bebbf8863d3f
Dax Borde <daxborde@gmail.com>
2020-12-22 16:18:24 -0500

    clean up comments

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use std::env;
 
-// fn greet(req: &HttpRequest) -> impl Responder {
-//     let to = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", to)
-// }
-
-// fn main() {
-//     // Get the port number to listen on.
-//     let port = env::var("PORT")
-//         .unwrap_or_else(|_| "3000".to_string())
-//         .parse()
-//         .expect("PORT must be a number");
-
-//     // Start a server, configuring the resources to serve.
-//     server::new(|| {
-//         App::new()
-//             .resource("/", |r| r.f(greet))
-//             .resource("/{name}", |r| r.f(greet))
-//     })
-//     .bind(("0.0.0.0", port))
-//     .expect("Can not bind to port 8000")
-//     .run();
-// }
-
-
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
